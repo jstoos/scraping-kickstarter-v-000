1c77d0b64057a0b8bcf22dc9b140ab409021727a
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-21 21:37:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   #project.css("p.bbcard_blurb").text
   #project.css("ul.project-meta li a span.location-name").text
-  #project.css("ul.project-stats li.first.funded strong").text
+  #project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 
 end
