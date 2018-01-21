45b0b8b8dc0562e126ff0fce0e203dc9b516c6ce
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-21 21:42:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,19 @@
 require 'nokogiri'
 require "pry"
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+
+    projects = {}
+
+    kickstarter.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
+  end
+
+# return the projects hash
+projects
 
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
